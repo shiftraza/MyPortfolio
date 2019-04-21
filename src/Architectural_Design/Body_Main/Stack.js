3de1ad059e30949f39d7c60d5ba48f98d9cdaa88
Armghan Haider <a32haide@uwaterloo.ca>
2019-04-20 22:20:15 -0400

    New commit

--- src/Architectural_Design/Body_Main/Stack.js
+++ src/Architectural_Design/Body_Main/Stack.js
@@ -1,44 +1,15 @@
 import React from "react";
 import { Spring } from "react-spring/renderprops";
+import Resume from "../../images/PDF/Armghan_Haider_Resume_2019.pdf";
 
 export default function Component1() {
   return (
-    <Spring
-      from={{ opacity: 0, marginLeft: -500 }}
-      to={{ opacity: 1, marginLeft: 0 }}
-      config={{ duration: 2000 }}
-    >
-      {props => (
-        <div style={props}>
-          <div>
-            <h1 className="Name">Armghan Haider</h1>
-            <Spring
-              from={{ number: 0 }}
-              to={{ number: 10 }}
-              config={{ duration: 10000 }}
-            >
-              {props => (
-                <div style={props}>
-                  <h1>{props.number.toFixed()}</h1>
-                </div>
-              )}
-            </Spring>
-          </div>
-        </div>
-      )}
-    </Spring>
+    <div className={"resume-container"}>
+      <div id={"resume-pdf"}>
+        You can view, download and print my résumé in pdf format in{" "}
+        <a href={"./public/resume_english.pdf"}>English</a> and{" "}
+        <a href={"#"}>Spanish</a>
+      </div>
+    </div>
   );
 }
-
-const c1Style = {
-  color: "red",
-  padding: "1.5rem"
-};
-
-const counter = {
-  background: "#333",
-  textAlign: "center",
-  width: "100px",
-  borderRadius: "50%",
-  margin: "1rem auto"
-};
