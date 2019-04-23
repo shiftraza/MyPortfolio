fc661c2d22b72561620f84e2fd0323e8fc513977
Armghan Haider <a32haide@uwaterloo.ca>
2019-04-23 03:40:55 -0400

    I have been successfully able to add content to projects and also get rid of horizontal overflow

--- src/Architectural_Design/views/Info/Paragraph.js
+++ src/Architectural_Design/views/Info/Paragraph.js
@@ -3,7 +3,7 @@ import "./Info.css";
 import picture from "../../../images/img/CarProject.PNG";
 const Paragraph = props => (
   <div className="container">
-    <h1 className="title">{props.header}</h1>
+    <div className="title">{props.header}</div>
     <p className="paragraph">{props.paragraph}</p>
     <img className="pictures" src={picture} />
   </div>
--- src/Architectural_Design/views/Projects/Projects.js
+++ src/Architectural_Design/views/Projects/Projects.js
@@ -7,12 +7,30 @@ import Paragraph from "../../views/Info/Paragraph";
 import "../Info/Info.css";
 class Projects extends Component {
   render() {
+    const paragraph =
+      "This project was done to gain a deeper understanding of electrical design as well as PID programming. This project involved setting up PID control Algorithm that would allow this robot to roam freely whilst detecting its surrounding. Once a wall is detected, the robot would turn and follow the wall at a pre-set distance. The PID control is done to ensure that the vehicle adheres to the specified distance. This programming was done using Arduino C code. ";
+    const header = "Autonomous Vehicle Project";
     return (
-      <div>
-        <Header />
-        <div className="container1">
-          <Video2 />
-          <Paragraph />
+      <div className="master">
+        <div className="main">
+          <Header />
+          <div className="container1">
+            <div id="test1">
+              <Paragraph paragraph={paragraph} header={header} />
+            </div>
+            <div id="test2">
+              <Paragraph paragraph={paragraph} header={header} />
+            </div>
+            <div id="test3">
+              <Paragraph paragraph={paragraph} header={header} />
+            </div>
+            <div id="test4">
+              <Paragraph paragraph={paragraph} header={header} />
+            </div>
+            <div className="video_test">
+              <Video2 />
+            </div>
+          </div>
         </div>
       </div>
     );
