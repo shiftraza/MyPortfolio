6d04165d21d6fa17626fd3a08c23755c166f20f6
Armghan Haider <a32haide@uwaterloo.ca>
2020-01-04 21:22:35 -0700

    AGV partical update

--- src/Architectural_Design/views/Projects/Proj_dets/AGV.js
+++ src/Architectural_Design/views/Projects/Proj_dets/AGV.js
@@ -34,12 +34,23 @@ class AGV extends Component {
           <h1 className="project_">Autonomous Guided Vehicle</h1>
           <div className="content">
             <p className="description">
-              blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
+              An AGV is self-driving vehicle which utilizes onboard logic to
+              travel in a specified path. These vehicles are very useful in
+              manufacturing facilities because it allows for parts to be
+              delivered to their locations quickly and efficiently. This also
+              removes the human element in part delivery which is important for
+              reducing cost and errors. The ultimate goal of this project was to
+              create an AGV which is capable of detecting obstacles in its path
+              and then use control logic to avoid collision whilst sticking to a
+              specified path.
             </p>
             <img className="pic1" src={Sunset} />
           </div>
           <p className="second_paragraph">
-            blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
+            Due to limited resources, the entire project was first
+            conceptualized on SolidWorks to reduce material cost. The AGV used a
+            12V battery with was then stepped down to meet the demands of 5V
+            sensors as well as fully power 4 , 12V DC motors.
           </p>
           <div className="pic_container">
             <img className="pic" src={Sunset} />
@@ -49,15 +60,24 @@ class AGV extends Component {
           <div className="contraint_container">
             <div className="contraint">
               <h1>Purpose</h1>
-              123blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
+              The purpose of this project was to create a functional AGV which
+              would autonomously drive and actively avoid objects such as shoes,
+              walls, etc.
             </div>
             <div className="contraint">
               <h1>Approach</h1>
-              123blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
+              My approach was to use 2 Ultrasonic sensors to monitor the
+              surrounding of the vehicle whilst using PID control logic to slow
+              down the vehicle and redirect it around the obstacle. I decided to
+              use cheaply available and easily replaceable motors to power the
+              vehicle to ensure ease of maintenance.
             </div>
             <div className="contraint">
               <h1>Results</h1>
-              123blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
+              The AGV concept effectively avoided obstacles and was an overall
+              success. The vehicle could effectively avoid obstacles within a
+              180-degree radius at the front and then monitor the obstacle from
+              the side sensor to prevent collisions.
             </div>
           </div>
           <div className="pic_container">
